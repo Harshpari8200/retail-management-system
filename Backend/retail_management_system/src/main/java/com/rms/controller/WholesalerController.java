b7d560157ef61dd82f3802e14e68eb5c5b40dffe
Jigar Dabhi <dabhijigar28@gmail.com>
2026-03-27 01:39:26 +0530

    controller fix

--- Backend/retail_management_system/src/main/java/com/rms/controller/WholesalerController.java
+++ Backend/retail_management_system/src/main/java/com/rms/controller/WholesalerController.java
@@ -13,7 +13,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/wholesaler")
+@RequestMapping("/api/wholesaler")
 @RequiredArgsConstructor
 public class WholesalerController {
 
